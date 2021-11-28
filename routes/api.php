360a42e3381891bfafa66d7b7b95d0640ee749f2
marco1730417 <marco17304@gmail.com>
2021-11-28 13:34:22 -0500

    validacion api rest with token

--- routes/api.php
+++ routes/api.php
@@ -25,23 +25,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::prefix('auth')->group(function () {
-    //Prefijo V1, todo lo que este dentro de este grupo se accedera escribiendo v1 en el navegador, es decir /api/v1/*
+    //Prefijo auth , todo lo que este dentro de este grupo se accedera escribiendo auth  en el navegador, es decir /api/auth/
+    // ejemplo authenticate http://127.0.0.1:8000/api/auth/authenticate
+    
     Route::post('authenticate', [\App\Http\Controllers\Api\AuthController::class, 'authenticate'])->name('authenticate');
     Route::post('register', [\App\Http\Controllers\Api\AuthController::class, 'register'])->name('register');
   
-    Route::get('telefonos/{id} ', [\App\Http\Controllers\Api\TelefonosController::class, 'show'])->name('show');
-   
-  
       Route::group(['middleware' => ['jwt.verify']], function() {
-   //     Route::post('logout', [AuthController::class, 'logout']);
-   Route::get('telefonos', [\App\Http\Controllers\Api\TelefonosController::class, 'index'])->name('index');
-   
+        Route::get('telefonos', [\App\Http\Controllers\Api\TelefonosController::class, 'index'])->name('index');
+        Route::get('telefonos/{id} ', [\App\Http\Controllers\Api\TelefonosController::class, 'show'])->name('show');
         Route::post('logout', [\App\Http\Controllers\Api\AuthController::class, 'logout'])->name('logout');
-  
-       
-  /*       Route::post('get-user', [AuthController::class, 'getUser']);
-        Route::post('telefonos', [TelefonosController::class, 'store']);
-        Route::put('telefonos/{id}', [TelefonosController::class, 'update']);
-        Route::delete('telefonos/{id}', [TelefonosController::class, 'destroy']); */
+        Route::post('telefonos', [\App\Http\Controllers\Api\TelefonosController::class, 'store'])->name('store');
+        Route::put('telefonos/{id}', [\App\Http\Controllers\Api\TelefonosController::class, 'update'])->name('update');
+        Route::delete('telefonos/{id}', [\App\Http\Controllers\Api\TelefonosController::class, 'destroy'])->name('destroy');
     });  
 });
